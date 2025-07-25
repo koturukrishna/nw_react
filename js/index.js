940ef70c1fd69b7961f71464cf8802d9b062facc
krishna <krishnakoturu526@gmail.com>
2025-07-25 17:42:54 +0530

    changed js file

--- js/index.js
+++ js/index.js
@@ -59,3 +59,18 @@ function displayPrimeNumners(n) {
 const prm = displayPrimeNumners(50);
 
 console.log(prm);
+
+function sortDescending(arr) {
+  return arr.sort((a, b) => b - a);
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] < arr[j]) {
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+}
+
+console.log(sortDescending([5, 3, 8, 1, 2])); // [8, 5, 3, 2, 1]
